bef0b88fc5615ff9d0f22131d4e8e295740d3c97
Jack Boberg <jack.boberg@gmail.com>
2015-07-09 10:49:55 -0400

    Add nginx proxy for static files
    listen on unix socket instead of TCP

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 set -euo pipefail
-set -x
+
+export HOME=/root
+export TEMP_DIR=/root/tmp
+
+# Install nginx
+echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+sudo apt-get update -y
+sudo apt-get install -y nginx=1.8.0-1+trusty1
+rm /etc/nginx/nginx.conf
+cp /opt/modulus/conf/nginx.conf /etc/nginx/nginx.conf
+rm /etc/nginx/sites-enabled/default
+cp /opt/modulus/conf/nginx-default /etc/nginx/sites-enabled/default
+mkdir /var/lib/nginx/body
+mkdir /var/lib/nginx/proxy
+mkdir /var/lib/nginx/fastcgi
+mkdir /var/lib/nginx/uwsgi
+mkdir /var/lib/nginx/scgi
